d1f0b6051a7556e08e82943d2e636c30c0e1b36c
Gianni Mariani <owebeeone@gmail.com>
2025-10-25 19:44:32 +1100

    Bump version to v0.5.2

--- src/sdax/__init__.py
+++ src/sdax/__init__.py
@@ -13,7 +13,7 @@ from .sdax_core import (
 )
 from .tasks import AsyncTask, RetryableException, SdaxTaskGroup, TaskFunction
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 __all__ = [
     "AsyncTask",
